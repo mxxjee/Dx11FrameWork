3ca2f31fa7223dca9d112c89a9ceacaa932c37ae
mxxjee <kmj694288@gmail.com>
2025-09-29 20:45:09 +0900

    ADD: Transform Function / Rotation/Chase/Turn..

--- Engine/Private/CTransform.cpp
+++ Engine/Private/CTransform.cpp
@@ -70,6 +70,115 @@ void CTransform::Move(DIRECTION eDir, float fTimeDelta)
 
 }
 
+_float3 CTransform::Get_Scale()
+{
+	return _float3(
+		XMVectorGetX(XMVector3Length(Get_State(STATE::RIGHT))),//float 추출
+		XMVectorGetX(XMVector3Length(Get_State(STATE::UP))),
+		XMVectorGetX(XMVector3Length(Get_State(STATE::LOOK)))
+		);
+
+}
+
+void CTransform::Rotation(_fvector vAxis, _float fRadian)
+{
+	
+	//특정 축을 통한 x ,y, z 회전.
+	_matrix AxisRotationMat = XMMatrixRotationAxis(vAxis, fRadian);
+	_float3 vScale = Get_Scale();
+
+	//방향벡터이므로 , w=0
+	_vector vRight = XMVectorSet(1.f, 0.f, 0.f, 0.f) * vScale.x;
+	_vector vUp = XMVectorSet(0.f, 1.f, 0.f, 0.f) * vScale.y;
+	_vector vLook = XMVectorSet(0.f, 0.f, 1.f, 0.f) * vScale.z;
+
+	Set_State(STATE::RIGHT, XMVector3TransformNormal(vRight, AxisRotationMat));
+	Set_State(STATE::UP, XMVector3TransformNormal(vUp, AxisRotationMat));
+	Set_State(STATE::LOOK, XMVector3TransformNormal(vLook, AxisRotationMat));
+
+
+	
+}
+
+void CTransform::Rotation(_float3 fRadian)
+{
+	//x,y,z 쿼터니온 4원수회전
+	Matrix QuaternionMat = XMMatrixRotationRollPitchYaw(fRadian.x, fRadian.y, fRadian.z);
+	_float3 vScale= Get_Scale();			//크기유지
+	
+	//크기 유지
+	_vector vRight = XMVectorSet(1.f, 0.f, 0.f, 0.f)*vScale.x;
+	_vector vUp = XMVectorSet(0.f, 1.f, 0.f, 0.f)*vScale.y;
+	_vector vLook = XMVectorSet(0.f, 0.f, 1.f, 0.f)*vScale.z;
+
+	Set_State(STATE::RIGHT, XMVector3TransformNormal(vRight, QuaternionMat));
+	Set_State(STATE::UP, XMVector3TransformNormal(vUp, QuaternionMat));
+	Set_State(STATE::LOOK, XMVector3TransformNormal(vLook, QuaternionMat));
+
+
+}
+
+void CTransform::Turn(_fvector vAxis, _float fTimeDelta)
+{
+	//누적회전
+	//1.회전행렬구하기
+	_matrix		RotationMat = XMMatrixRotationAxis(vAxis, m_fRotationPerSec * fTimeDelta);
+	
+	//2. 나의 right/up/look구하기
+	_vector	vRight = Get_State(STATE::RIGHT);
+	_vector	vUp = Get_State(STATE::RIGHT);
+	_vector	vLook = Get_State(STATE::RIGHT);
+
+
+	//3.회전행렬과 계산
+	Set_State(STATE::RIGHT, XMVector3TransformNormal(vRight, RotationMat));
+	Set_State(STATE::UP, XMVector3TransformNormal(vUp, RotationMat));
+	Set_State(STATE::LOOK, XMVector3TransformNormal(vLook, RotationMat));
+
+
+}
+
+
+void CTransform::LookAt(_fvector vPoint)
+{
+	_float3 vScale = Get_Scale();
+
+	//new look 구하기
+	_vector vPosition = Get_State(STATE::POSITION);
+	
+	_vector vNewLook = vPoint - vPosition;
+	_vector vNewRight = XMVector3Cross(WORLD_UP, vNewLook);
+	_vector vNewUp = XMVector3Cross(vNewLook, vNewRight);
+
+
+	vNewRight = XMVector3Normalize(vNewRight) * vScale.x;
+	vNewUp = XMVector3Normalize(vNewUp) * vScale.y;
+	vNewLook = XMVector3Normalize(vNewLook) * vScale.z;
+
+
+	Set_State(STATE::RIGHT, vNewRight);
+	Set_State(STATE::UP, vNewUp);
+	Set_State(STATE::LOOK, vNewLook);
+
+
+}
+
+void CTransform::Chase(_fvector vPoint, _float fTimeDelta, _float MinDistance)
+{
+	//쫓아가는 방향구하기
+	_vector vPosition = Get_State(STATE::POSITION);
+	
+	_vector vNewLook = vPoint - vPosition;
+	_float vLookLength = XMVectorGetX(XMVector3Length(vNewLook));
+
+	
+	if (vLookLength >= MinDistance)
+		vPosition += XMVector3Normalize(vNewLook) * fTimeDelta * m_fSpeedPerSec;
+
+		
+	Set_State(STATE::POSITION, vPosition);
+
+}
 
 CTransform* CTransform::Create(ComPtr<ID3D11Device> pDevice, ComPtr<ID3D11DeviceContext> pContext)
 {
@@ -83,6 +192,8 @@ CTransform* CTransform::Create(ComPtr<ID3D11Device> pDevice, ComPtr<ID3D11Device
 
 	return pInstance;
 }
+
+
 
 CComponent* CTransform::Clone(void* pArg)
 {
--- Engine/Public/CTransform.h
+++ Engine/Public/CTransform.h
@@ -46,9 +46,21 @@ public:
     }
 
     const _float4x4& Get_World() { return m_WorldMatrix; }
+    _float3  Get_Scale();        //right.up.forward벡터 길이만 리턴하자.
+
+#pragma region Rotation
 public:
     void    Move(DIRECTION eDir, float fTimeDelta);
+    
+    void    Rotation(_fvector vAxis, _float fRadian);       //vAxis축을 기준으로 fRAdian만큼 회전시킨다. ( 누적값은 아니며 항상 항등상태에서 회전을 적용한다).
+    void    Rotation(_float3 fRadian);
 
+    void    Turn(_fvector vAxis, _float fTimeDelta);        //매프레임마다 vAxis축을 기준으로 회전
+
+    void    LookAt(_fvector vPoint);            //한 점을 바로 바라보도록회전
+    void    Chase(_fvector vPoint, _float fTimeDelta, _float MinDistance = 0.f);    //최소 거리 까지만 쫓아간다.
+
+#pragma endregion
 private:
     /*저장용 데이터( 직렬 계산)*/
     _float  m_fSpeedPerSec = {};
